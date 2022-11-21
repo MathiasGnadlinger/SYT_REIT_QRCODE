89d959037e5b792c1a5960961c5d42ce489dbfa6
MathiasGnadlinger <72551431+MathiasGnadlinger@users.noreply.github.com>
2022-11-21 10:46:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SYT_REIT_QRCODE
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-utoml6)
---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
+
+    <Suspense>
+      <template #default>
+        <UserSelectList />
+      </template>
+      <template #fallback>
+        <span>Loading...</span>
+      </template>
+    </Suspense>
+
+    <UserList />
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer.vue';
+import Main from './components/Main.vue';
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Footer,
+    Header,
+    Main,
+  },
+};
+</script>
+
+<style>
+img {
+  width: 5em;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  //text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
